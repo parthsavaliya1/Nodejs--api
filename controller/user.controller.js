1d21108ab9f7530272337706bdac830378bebedd
parthsavaliya111 <144902385+parthsavaliya111@users.noreply.github.com>
2023-09-13 17:13:39 +0530

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { createUser, getAllUserWithProduct } = require('../service/user.service');
+const User = require('../model/user');
+const authenticateToken = require('../config/authentication');
 const router = express.Router();
 
 router.post('/create', async (req, res) => {
@@ -22,7 +25,25 @@ router.post('/create', async (req, res) => {
     })
 })
 
-router.get('/get-user-by-product', async (req, res) => {
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+      const user =await User.findOne({userName:username});
+  
+    if (!user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+      const token = jwt.sign({ username: user.userName }, process.env.JWT_SECRET, {
+      expiresIn: '1h', 
+    });
+    res.json({ token });
+  });
+
+router.get('/get-user-by-product',authenticateToken, async (req, res) => {
     getAllUserWithProduct().then((resp) => {
         res.status(200).json(resp);
     }).catch((error) => {
